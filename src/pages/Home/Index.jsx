11275478452e301cbae1d0931a8634dfc76fb6db
Vitor de Paula <vitordcode@gmail.com>
2022-06-06 20:31:34 -0300

    Subindo aplicacao

---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export function Home() {
+
+  return (
+    <div className='container'>
+      <h1>Lista de presenca</h1>
+      <input type="text" placeholder="Digite seu nome..."/>
+      <button type="button">Adicionar</button>
+    </div>
+  )
+}
