9409a1dcca22251970492815ada7cf8de7eb1885
Vitor de Paula <vitordcode@gmail.com>
2022-06-12 19:28:02 -0300

    Criando Estados no Input

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,16 +1,24 @@
+import React, { useState } from 'react';
+
 import './styles.css';
 
 import { Card } from '../../components/Card';
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
 
   return (
     <div className='container'>
-      <h1>Lista de presenca</h1>
-      <input type="text" placeholder="Digite seu nome..."/>
+      <h1>Nome: {studentName}</h1>
+      <input 
+        type="text" 
+        placeholder="Digite seu nome..."
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
-      <Card />
+      <Card name="Vitor" time="10:55:25"/>
+      <Card name="Rodrigo" time="11:00:10"/>
     </div>
   )
 }
